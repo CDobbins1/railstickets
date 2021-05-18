a91fc8b5e8cc05eacd4382002c91cfe9ad99f4af
CDobbins1 <cdobsc01@hotmail.com>
2021-05-18 12:50:48 -0700

    added name field to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
-            devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :admin])
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password, :admin])
         end
 end
